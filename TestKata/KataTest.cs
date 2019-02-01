700d24092b43808a70cf029d66351fe2f28e5316
Christopher Parsons <mail@christopherparsons.me>
2019-02-01 15:01:14 +0000

    Initial Commit

---
+++ TestKata/KataTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Kata;
+using Xunit;
+
+namespace TestKata
+{
+    public class KataTest
+    {
+        [Fact]
+        public void It_Returns_A_String()
+        {
+            
+        }
+    }
+}
